a30b5d62549b2bb2c96b617f3ec70615ccef3543
Angeles <angeles.cahill@gmail.com>
2025-04-08 21:32:29 -0400

    Configuración de Jest y db.json actualizada

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
+// filepath: c:\backend-curso\desafios\0013-integracionesM8\jest.config.js
 export default {
-    testEnvironment: "node", 
-    transform: {}, 
-  };
+  testEnvironment: 'node',
+  transform: {},
+};
